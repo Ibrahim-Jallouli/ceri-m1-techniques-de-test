438351ad963fb5b9282c6ce67474dc73b237ab9c
Ibrahim-Jallouli <ibrahim.jallouli@alumni.univ-avignon.fr>
2024-04-08 11:07:50 +0200

    i litrally did nothing

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;g
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
