6b105496f9913fb027488f03a08ed5259f41c6e0
Ibrahim-Jallouli <ibrahim.jallouli@alumni.univ-avignon.fr>
2024-04-08 11:06:17 +0200

    tp 5

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -10,7 +10,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;g
 
 
 /**
